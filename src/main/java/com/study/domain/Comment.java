09f5d3e40d3c783228ece5481525670e8ee704b1
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-01-29 17:02:02 +0900

    Feat : CommentDTO 생성

---
+++ src/main/java/com/study/domain/Comment.java
@@ -0,0 +1,42 @@
+package com.study.domain;
+
+import java.sql.Timestamp;
+
+public class Comment {
+    private int commentId;
+    private int boardId;
+    private String comment;
+    private Timestamp createdAt;
+
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public int getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(int boardId) {
+        this.boardId = boardId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
