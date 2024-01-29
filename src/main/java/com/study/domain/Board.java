06b2819379dd379fe0d4c75e6634f6258cb9ce4e
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-01-29 16:58:36 +0900

    Feat : BoardDTO 생성

---
+++ src/main/java/com/study/domain/Board.java
@@ -0,0 +1,78 @@
+package com.study.domain;
+
+import java.sql.Timestamp;
+
+public class Board {
+    private int boardId;
+    private String title;
+    private int views;
+    private Timestamp createdAt;
+    private Timestamp editedAt;
+    private String content;
+    private String userName;
+    private String password;
+
+    public int getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(int boardId) {
+        this.boardId = boardId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getEditedAt() {
+        return editedAt;
+    }
+
+    public void setEditedAt(Timestamp editedAt) {
+        this.editedAt = editedAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
