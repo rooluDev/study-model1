49d576bf0a4df1f71822297a319976e65e187f6a
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2024-01-29 17:01:14 +0900

    Feat : CategoryDTO 생성

---
+++ src/main/java/com/study/domain/Category.java
@@ -0,0 +1,22 @@
+package com.study.domain;
+
+public class Category {
+    private int boardId;
+    private int categoryName;
+
+    public int getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(int boardId) {
+        this.boardId = boardId;
+    }
+
+    public int getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(int categoryName) {
+        this.categoryName = categoryName;
+    }
+}
